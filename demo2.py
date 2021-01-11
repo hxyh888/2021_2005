f76d38d0ae93390ee390b137424de39cf941b350
hxyh <hxyhsoft@163.com>
2021-01-11 14:41:20 +0800

    新增得脉

---
+++ demo2.py
@@ -0,0 +1,19 @@
+git命令
+1.git init  初始化本地仓库
+2.git add . 添加文件
+3.git commit -m""   提交文件到暂存区
+4.git status    查看分支状态
+5.git diff  查看不同
+6.git log   查看日志
+7.git checkout -- 文件名 撤销
+8.git reset -- hard HEAD    回退版本
+9.git rm 文件名    删除文件
+10.git remote add origin 仓库地址   本地仓库和远端仓库关联
+11.git remote 查看关联的仓库
+12.git remote rm 仓库名字   删除管理仓库
+13.git checkout -b 分支名  创建并且切换分支
+14.git push -u orogin master    推送到远端分支
+15.git merger   合入
+16.git clone    从远端克隆到本地
+17.git pull 跟新代码
+18.git switch   创建分支
